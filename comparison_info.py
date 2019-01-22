c5e65da2bd915530f5aed5d403615591bb226cf3
Michal <michal.thor5@gmail.com>
2019-01-22 12:46:31 +0100

    Add comparison_info

---
+++ comparison_info.py
@@ -0,0 +1,22 @@
+import sqlite3
+import pandas as pd
+
+def ComparisonInfo():
+    conn = sqlite3.connect('project_database.db')
+    query = """SELECT
+               cl.full_name as company_name,
+               max(high) as max_price,
+               min(low) as min_price,
+               max(ch.date) as till_date,
+               min(ch.date) as from_date,
+               avg(volume) as mean_volume
+               from company_hist ch
+               left join company_list cl
+               on ch.company=cl.short_name
+               group by company"""
+    results = pd.read_sql(query,conn)
+    conn.close()
+    return results
+
+
+print(ComparisonInfo())
